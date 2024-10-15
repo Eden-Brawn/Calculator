7992090c3f5fb7661fe924e47f31540227346e48
Eden Brawn <edenbrawn@gmail.com>
2024-10-15 19:35:10 +0100

    added eval to do math but only works for two buttons

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showOnScreen(x){
     document.getElementById('screen').innerHTML += x;
 }
+function calculation() { 
+    var x = document.getElementById('screen').innerHTML;
+    var y = eval(x);
+    document.getElementById('screen').innerHTML = y;
+} 
