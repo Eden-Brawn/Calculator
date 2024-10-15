7be113db817ac84b71de6fe19fb14811f0814749
Eden Brawn <edenbrawn@gmail.com>
2024-10-15 19:42:34 +0100

    added comments explaining the two functions

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+/*This puts the value on screen that is submitted as x*/
 function showOnScreen(x){
     document.getElementById('screen').innerHTML += x;
 }
+/*This carrys out the calculation inputted in to the screen*/
 function calculation() { 
     var x = document.getElementById('screen').innerHTML;
-    var y = eval(x);
+    var y = eval(x);/*eval from https://www.shecodes.io/athena/169245-javascript-function-to-evaluate-strings*/
     document.getElementById('screen').innerHTML = y;
 } 
