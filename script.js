f62e011f0334005d49c812377c7952b0106e5a21
Eden Brawn <edenbrawn@gmail.com>
2024-10-16 17:21:05 +0100

    added a clear button

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ function showOnScreen(x){
 }
 /*This carrys out the calculation inputted in to the screen*/
 function calculation() { 
-    var x = document.getElementById('screen').innerHTML;
-    var y = eval(x);/*eval from https://www.shecodes.io/athena/169245-javascript-function-to-evaluate-strings*/
+    let x = document.getElementById('screen').innerHTML;
+    let y = eval(x);/*eval from https://www.shecodes.io/athena/169245-javascript-function-to-evaluate-strings*/
     document.getElementById('screen').innerHTML = y;
 } 
+/*This clears the screen*/
+function clearCalc(){
+    document.getElementById('screen').innerHTML = "";
+}
